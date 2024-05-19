4c563e77b2d3c51162c5f1e8aab15f885fb779c2
oneloveoneheart <gahetaflorent@gmail.com>
2024-05-19 18:48:44 +0200

    I managed to generate the password

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const rangeInputElement = document.getElementById("rangeInput");
 const numberInputElement = document.getElementById("numberInput");
 const form = document.getElementById("passwordGeneratorForm");
-const includeUppercaseElement = document.getElementById("includeUppercase");
-const includeNumbersElement = document.getElementById("includeNumbers");
-const includeSymbolsElement = document.getElementById("includeSymbols");
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
@@ -17,13 +14,40 @@ rangeInputElement.addEventListener("input", syncCharacterAmount);
 numberInputElement.addEventListener("input", syncCharacterAmount);
 form.addEventListener("submit", event =>{
     event.preventDefault();
-    const includeUppercase = includeUppercaseElement.checked;
-    const includeNumbers = includeNumbersElement.checked;
-    const includeSymbols = includeSymbolsElement.checked;
     const characterAmount = numberInputElement.value;
-    const password = generatePassWord(characterAmount, includeUppercase, includeNumbers, includeSymbols);
+    console.log("character amount : " + characterAmount);
+    const password = generatePassWord(characterAmount);
+    console.log("password length: " + password.length);
+    console.log("password : " + password);
 
 });
+
+function generatePassWord(characterAmount) {
+    const baseLength = Math.floor(characterAmount / 4);
+    const remainder = characterAmount % 4;
+
+    const lowerCaseCount = remainder > 0? baseLength + 1: baseLength;
+    const upperCaseCount = remainder > 1? baseLength + 1: baseLength;
+    const numberCount = remainder > 2? baseLength + 1: baseLength;
+
+    let password = [].concat(getRandomCharacters(LOWERCASE_CHAR_CODES, lowerCaseCount))
+        .concat(getRandomCharacters(UPPERCASE_CHAR_CODES, upperCaseCount))
+        .concat(getRandomCharacters(NUMBER_CHAR_CODES, numberCount))
+        .concat(getRandomCharacters(SYMBOL_CHAR_CODES, baseLength));
+
+    password.sort(() => Math.random() - 0.5);
+
+    return password.join('');
+}
+
+function getRandomCharacters(charCodeArray, count) {
+    let result = [];
+    for (let i = 0; i < count; i++) {
+        const randomCode = charCodeArray[Math.floor(Math.random() * charCodeArray.length)];
+        result.push(String.fromCharCode(randomCode));
+    }
+    return result;
+}
 
 function arrayFromLowToHigh(low, high){
     const array = []
